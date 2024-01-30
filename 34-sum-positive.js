dd039896eb9fdcc1bced56da521788eede3e3bd0
Matías Rodlo <m@hablaqui.com>
2024-01-30 17:19:59 -0300

    sum-positive

---
+++ 34-sum-positive.js
@@ -0,0 +1,23 @@
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+function positiveSum(arr) {
+  let positive = [];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      positive.push(arr[i]);
+    }
+  }
+  for (let j = 0; j < positive.length; j++) {
+    sum += positive[j];
+  }
+  return sum;
+}
+
+positiveSum([1, -4, 7, 12]);

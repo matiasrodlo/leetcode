65538b44209bf014cea4de5f951d9d47d802a964
Matías Rodlo <m@hablaqui.com>
2024-01-30 10:29:08 -0300

    average array taka

---
+++ 19-clock.js
@@ -0,0 +1,5 @@
+function past(h, m, s) {
+  return 3600000 * h + 60000 * m + s * 1000;
+}
+
+past(1, 1, 1);
---
+++ 20-calculate-average.js
@@ -0,0 +1,19 @@
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+ */
+
+function findAverage(array) {
+  let sum = 0;
+  if (array !== 0) {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  } else {
+    return 0;
+  }
+}
+
+findAverage([]);

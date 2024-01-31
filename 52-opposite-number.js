88cdb6468dd45c1a50f696b4f7fd1d88f780a34b
Matías Rodlo <m@hablaqui.com>
2024-01-31 14:17:35 -0300

    kata: opposite num

---
+++ 52-opposite-number.js
@@ -0,0 +1,15 @@
+/*
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+  return -number;
+}
+
+opposite(-2);

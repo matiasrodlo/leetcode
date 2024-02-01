c9123e12f9657582f69683443a2e2bb2a8c3535b
Matías Rodlo <m@hablaqui.com>
2024-02-01 16:33:36 -0300

    average array

---
+++ 77-average-grades.js
@@ -0,0 +1,19 @@
+/*
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+
+function getAverage(marks) {
+  let sum = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+  let result = Math.floor(sum / marks.length);
+  return result;
+}
+
+getAverage([2, 6, 2, 2]);

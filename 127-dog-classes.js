2b1967e07dfa033818610098599676e74d64fe6b
Matias Rodlo <m@hablaqui.com>
2024-02-13 14:11:12 -0300

    dog classes

---
+++ 127-dog-classes.js
@@ -0,0 +1,46 @@
+/*
+Objective
+Preloaded for you is a class Dog:
+
+class Dog {
+  constructor(name, age, gender, species, size, master, loyal) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = species;
+    this.legs = 4;
+    this.size = size;
+    this.master = master;
+    this.loyal = loyal;
+  }
+}
+You are then given a working class Labrador as your initial code.
+
+class Labrador {
+  constructor(name, age, gender, master) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = master;
+    this.loyal = true;
+  }
+}
+Shorten it so that it meets the strict character count requirements for this Kata.
+
+Quick, get your timer out and get ready to time yourself. Are you ready? Ready, get set, GO!!! :D
+*/
+
+class Labrador{constructor(n,a,g,m) {
+    this.name=n;
+    this.age=a;
+    this.gender=g;
+    this.species="Labrador";
+    this.legs=4;
+    this.size="Large";
+    this.master=m;
+    this.loyal=true;
+   }
+ }

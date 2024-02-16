535dafa9889119b632469fffabdc1d3c3c0369eb
Matias Rodlo <m@hablaqui.com>
2024-02-15 21:55:37 -0300

    combine strings

---
+++ 133-combine-string.js
@@ -0,0 +1,19 @@
+/*
+
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+*/
+
+const combineNames = (firstName, lastName) => {
+    return `${firstName} ${lastName}`
+  }
+  
+  combineNames('matias', 'rodlo')

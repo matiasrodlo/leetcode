469646ec617e2bec2106f2f01cbe2abee4fa3282
Matías Rodlo <m@hablaqui.com>
2024-06-09 18:32:29 -0400

    number that its include in array

---
+++ 191-number-includes.js
@@ -0,0 +1,9 @@
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item) {
+  return arr.includes(item);
+}
+
+include([1, 2, 3, 4], 7);

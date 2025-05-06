268bec2a9b728e31a33592c04e2a7778213dabd6
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-05-06 13:13:35 -0400

    Create 12-sort-sum.py

---
+++ python/12-sort-sum.py
@@ -0,0 +1,19 @@
+"""
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+"""
+
+def sum_array(arr):
+    if arr is None or len(arr) < 3:
+        return 0 
+    sorted_numbers = sorted(arr)
+    return sum(sorted_numbers[1:-1]) 
